388710a6d20fc1296225498637ce5cdce7647367
jithin <jithin@jithin.space>
2018-01-04 14:59:06 +0530

    joubelui not working

--- js/h5p-image-grid.js
+++ js/h5p-image-grid.js
@@ -1,6 +1,6 @@
 
 //code starts here
-H5P.ImageGrid = (function($){
+H5P.ImageGrid = (function($,UI){
 
         //variable declaration part
         var self = this;
@@ -17,7 +17,7 @@ H5P.ImageGrid = (function($){
                 var imageWidth = params.image.width;
                 var margin = 2;
                 var createImageGrid = function(){
-    
+
                         var gridWidth = parseInt(imageWidth)+(margin*level*2);
                         var gridHeight = parseInt(imageHeight)+(margin*level*2);
 
@@ -28,49 +28,96 @@ H5P.ImageGrid = (function($){
                                 {
                                         fragmentList.push(new ImageGrid.Fragment(fragmentWidth,fragmentHeight,i,j));
                                  }
-                                                   
-                                
+
+
                          }
 
 
                 }
                 var imageLoaded = function(){
                         //
-                        console.log("calling");
+                        // console.log("calling");
                         createImageGrid();
 
+                }
+
+                self.constructGrid = function(){
+
+                var $grid = $('<ul class="grid-container" >');
+
+                  for(var i=0;i<level;i++){
+
+                    var $row = $('<div class="row" />');
+                    for(var j=0;j<level;j++){
+
+                      var $element= $("<li />").
+                                   css("background-position","-" + (fragmentList[i*level+j].width*fragmentList[i*level+j].col) +"px -" +(fragmentList[i*level+j].height*fragmentList[i*level+j].row) +"px")
+                                   .css("width", fragmentWidth+"px")
+                                   .css("height", fragmentHeight+"px")
+                                   .css("background-image", "url("+H5P.getPath(self.params.image.path,self.id)+")")
+                                   .css("display", "inline-block")
+                                   .css("margin", 1);
+
+
+
+                      $row.append($element);
+
+                    }
+                    $grid.append($row)
+                  }
+
+                  return $grid;
                 }
 
 
                 //when image loaded
                 imageLoaded();
-                H5P.shuffleArray(fragmentList);
+                // H5P.shuffleArray(fragmentList);
 
         };
 
         ImageGrid.prototype.attach = function($container){
                 //attach function
                 $container.addClass('h5p-image-grid');
-                $container.append('<img src="'+ H5P.getPath(self.params.image.path,self.id)+'"/>');
-                var $grid = $('<ul class="grid-container" >');
-                 for(var i=0;i<fragmentList.length;i++){
+                // $container.append('<img src="'+ H5P.getPath(self.params.image.path,self.id)+'"/>');
 
-                        var $element= $("<li />").
-                                css("background-position","-" + (fragmentList[i].width*fragmentList[i].col) +"px -" +(fragmentList[i].height*fragmentList[i].row) +"px")
-                                .css("width", fragmentWidth+"px")
-                                .css("height", fragmentHeight+"px")
-                                .css("background-image", "url("+H5P.getPath(self.params.image.path,self.id)+")")
-                                .css("display", "inline-block")
-                                .css("margin", 2);
-                
-                        $grid.append($element);
-                } 
 
-               
+               var $grid=self.constructGrid();
+
+
+
+                //ifuser can select the difficulty
+                // code goes here
+
+                //allow fullscreen
+
+                //display the start puzzle button
+
+        //         self.$submit = UI.createButton({
+        //                     title: 'Submit',
+        //   click: function(event) {
+        //     var order = $list.sortable("toArray");
+        //     gameSubmitted(order, $list);
+        //   },
+        //   html: '<span><i class="fa fa-check" aria-hidden="true"></i></span>&nbsp;' + parameters.l10n.checkAnswer
+        // });
+        // self.$submit.appendTo(self.$buttonContainer);
+
+                var $startPuzzleButton = UI.createButton({
+                  title: 'Submit',
+                  click: function(event){
+                    alert("working");
+                  },
+                  html: 'Start the puzzle'
+                });
+
                 $container.append($grid);
+                $container.append($startPuzzleButton);
+
+
                 H5P.trigger("resize");
-                
+
         }
 
         return ImageGrid;
-})(H5P.jQuery);
+})(H5P.jQuery,H5P.JoubelUI);
